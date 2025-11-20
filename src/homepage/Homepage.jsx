6c41fa6ba19a30602f9a6e0dd5a74d2c3f600743
Burman <bbedford@charlotte.edu>
2025-11-20 14:55:00 -0500

    Added CSS for homepage and blog post

--- src/homepage/Homepage.jsx
+++ src/homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useUsername } from "../authWrapper/AuthContext";
+import { Link } from "react-router";
 
 
 function Homepage() {
@@ -8,10 +9,23 @@ function Homepage() {
     const loggedInUser = `Welcome ${username}, Please navigate to the Blog Post to start Blogging`
     const loggedIn = username ? loggedInUser : loggedOutUser; 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen gap-y-4 bg-gray-50">
-            <h1 className="text-2xl font-semibold text-gray-800 text-center bg-white shadow-md p-6 rounded-xl max-w-xl">
-                {loggedIn}
-            </h1>
+        <div className="flex flex-col min-h-screen bg-gray-50">
+            <main className="flex-1 flex flex-col items-center gap-y-4 px-4 md:px-0 pt-10 pb-10">
+                <div className="bg-white w-full max-w-2xl shadow-md text-center p-6 sm:p-10 rounded-2xl border border-gray-200">
+                    <h1 className = "text-xl sm:text-2xl font-semibold text-gray-800 mb-4 sm:mb-6">My Blog</h1> 
+                    <div className="bg-white shadow-md border border-gray-100 rounded-xl p-4 sm:p-6 max-w-xl mx-auto text-gray-700 text-base leading-relaxed">
+                        {loggedIn}
+                    </div>
+
+                    <div className= "flex flex-col sm:flex-row gap-4 mt-6 justify-center">
+                        <Link to="/posts" className="bg-blue-400 w-full sm:w-fit px-8 py-2 rounded-2xl text-white text-center">Blog Posts</Link>
+                        {!username &&
+                            (<Link to="/login" className="bg-green-400 w-full sm:w-fit px-8 py-2 rounded-2xl text-white text-center">Login</Link>)
+                        }
+                    </div>
+
+                </div>
+            </main>
         </div>
         );
     }
--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -19,12 +19,17 @@ function Login() {
         navigate('/posts');
     }
     return (
-        <div className=' max-w-96 m-12'>
-            <form className='flex flex-col gap-4' onSubmit={onSubmit}>
-            <input required placeholder='Username' className='border' value={userData.username} onChange={e => setUserData({ ...userData, username: e.target.value }) } />
-            <input required placeholder='Password' type='password' className ='border' value={userData.password} onChange={e => setUserData({ ...userData, password: e.target.value }) } />
-            <button type='submit' className='bg-blue-400 w-fit px-8 py-2 rounded-2xl text-white'>Submit</button>
-            </form>
+        <div className = 'flex justify-center items-start min-h-screen bg-gray-100 px-4 pt-20'>
+            <div className  = 'w-full max-w-lg bg-white rounded-2xl shadow-md border border-gray-200 p-8'>
+                <h1 className='text-3xl font-bold mb-6 text-center'>Login</h1>
+
+                    <form className='flex flex-col gap-5' onSubmit={onSubmit}>
+                    <input required placeholder='Username' className='border rounded px-4 py-2' value={userData.username} onChange={e => setUserData({ ...userData, username: e.target.value }) } />
+                    <input required placeholder='Password' type='password' className ='border rounded px-4 py-2' value={userData.password} onChange={e => setUserData({ ...userData, password: e.target.value }) } />
+                    <button type='submit' className='bg-yellow-400 w-fit px-8 py-2 rounded-2xl text-white mx-auto'>Submit</button>
+                    </form>
+                
+            </div>
         </div>
     )
 }
